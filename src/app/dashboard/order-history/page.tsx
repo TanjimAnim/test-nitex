378aa8d073ea13e5183f0d8fa5f0dd8f42dc2e83
TanjimAnim <tanjimanim007@gmail.com>
2023-12-01 23:27:34 +0600

    feat: protected path

--- src/app/dashboard/order-history/page.tsx
+++ src/app/dashboard/order-history/page.tsx
@@ -1,3 +1,11 @@
+import { getRequestCookie } from "@/libs/getRequestCookie";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+
 export default async function OrderHistory() {
+  const user = await getRequestCookie(cookies());
+  if (!user) {
+    redirect("/login");
+  }
   return <>this is order history page</>;
 }
--- src/app/dashboard/payment/page.tsx
+++ src/app/dashboard/payment/page.tsx
@@ -1,6 +1,13 @@
 import PaymentComponent from "@/components/Payment";
+import { getRequestCookie } from "@/libs/getRequestCookie";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Payment() {
+  const user = await getRequestCookie(cookies());
+  if (!user) {
+    redirect("/login");
+  }
   return (
     <div>
       <PaymentComponent />
